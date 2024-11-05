db9ea816ac78a0537f542ff0330d331071845a16
jcharsha2003 <129018057+jcharsha2003@users.noreply.github.com>
2024-11-05 21:04:59 +0530

    Update UserLoginContextStore.js

--- client/src/context/UserLoginContextStore.js
+++ client/src/context/UserLoginContextStore.js
@@ -15,7 +15,7 @@ function UserLoginContextStore({children}){
     
     // userlogin 
     const loginUser=(userCredObj)=>{
-        axios.post(`/user-api/user-login`,userCredObj)
+        axios.post(`https://emp-test.onrender.com/user-api/user-login`,userCredObj)
         .then(response=>{
             if(response.data.message==="success"){
                 setCurrentUser({...response.data.user})
